ce99c475ec8b9d46b4b242203ef89fa3cc44b553
Yacoubbeltanivelle <yacoubbeltanivelle@gmail.com>
2025-05-14 11:01:05 +0200

    ACTIVITE 9 : Mettre en place la requête d’inscription

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,26 +1,73 @@
 import { useState } from "react";
 import { Form, Button, Container, Card, Row, Col } from "react-bootstrap";
 
+import { useNavigate } from "react-router";
+
 const Register = () => {
   const [formData, setFormData] = useState({
     email: "",
     name: "",
     password: "",
   });
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
     });
+    console.log("Form submitted:", formData);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Handle signup logic here
-    // Don't forget to handle errors, both for yourself (dev) and for the client (via a Bootstrap Alert)
-    // Redirect to Login on success
-    console.log("Form submitted:", formData);
+
+    if (!formData.email.includes("@")) {
+      alert("Veuillez entrer une adresse email valide.");
+      return;
+    }
+
+    if (formData.name.trim().length < 3) {
+      alert("Le nom doit contenir au moins 3 caractères.");
+      return;
+    }
+
+    if (formData.password.length < 8) {
+      alert("Le mot de passe doit contenir au moins 8 caractères.");
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        "https://offers-api.digistos.com/api/auth/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        const errorMessage = errorData.message || "Une erreur est survenue.";
+        throw new Error(
+          `Erreur HTTP: ${
+            response.statusText ? response.statusText + " - " : ""
+          }${response.status} - ${errorMessage}`
+        );
+      }
+
+      const data = await response.json();
+      alert(`${data.name} a été créé`);
+      console.log("Form submitted:", formData);
+      navigate("/connexion");
+    } catch (err) {
+      console.log(err.message);
+      alert(`L'utilisateur n'a pu être ajouté: ${err.message}`);
+    }
   };
 
   return (
